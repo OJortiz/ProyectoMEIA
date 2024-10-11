21a4ccfc22ac0ececb24c5797429f42404b6753c
Mario <3943008660101@ingenieria.usac.edu.gt>
2024-10-10 20:45:43 -0600

    Agregar Form de Nuevo Usuario

---
+++ ProyectoMEIA/ProyectoMEIA/NuevoUsuario.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ProyectoMEIA
+{
+    partial class NuevoUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            txtUsuario = new TextBox();
+            txtNombre = new TextBox();
+            txtApellido = new TextBox();
+            txtPassword = new TextBox();
+            txtTelefono = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            btnRegistrar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(98, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(98, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(98, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Apellido: ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(98, 186);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Contraseña: ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(98, 237);
+            label5.Name = "label5";
+            label5.Size = new Size(152, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Fecha de Nacimiento:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(98, 287);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Telefono:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(333, 12);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(125, 27);
+            txtUsuario.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(333, 72);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(125, 27);
+            txtNombre.TabIndex = 7;
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(333, 125);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(125, 27);
+            txtApellido.TabIndex = 8;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(333, 179);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(125, 27);
+            txtPassword.TabIndex = 9;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Location = new Point(333, 280);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.Size = new Size(125, 27);
+            txtTelefono.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(333, 232);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(125, 27);
+            dateTimePicker1.TabIndex = 11;
+            dateTimePicker1.Value = new DateTime(2024, 10, 10, 0, 0, 0, 0);
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(247, 343);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(94, 29);
+            btnRegistrar.TabIndex = 12;
+            btnRegistrar.Text = "Registrarse";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            btnRegistrar.Click += btnRegistrar_Click;
+            // 
+            // NuevoUsuario
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(635, 450);
+            Controls.Add(btnRegistrar);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(txtTelefono);
+            Controls.Add(txtPassword);
+            Controls.Add(txtApellido);
+            Controls.Add(txtNombre);
+            Controls.Add(txtUsuario);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "NuevoUsuario";
+            Text = "NuevoUsuario";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox txtUsuario;
+        private TextBox txtNombre;
+        private TextBox txtApellido;
+        private TextBox txtPassword;
+        private TextBox txtTelefono;
+        private DateTimePicker dateTimePicker1;
+        private Button btnRegistrar;
+    }
+}
