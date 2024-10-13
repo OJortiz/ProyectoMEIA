f9f5cfb4bdb90985bc57efaee4fe64575a171b6a
Mario <3943008660101@ingenieria.usac.edu.gt>
2024-10-13 02:39:07 -0600

    Creacion de menu para usuarios

---
+++ ProyectoMEIA/ProyectoMEIA/ActualizacionUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoMEIA
+{
+    public partial class ActualizacionUsuario : Form
+    {
+        public ActualizacionUsuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
