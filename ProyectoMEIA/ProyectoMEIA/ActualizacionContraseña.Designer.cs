3dc5189fb5cbd33e7b044d05fcbd62d3e5f96f45
Mario <3943008660101@ingenieria.usac.edu.gt>
2024-10-13 11:56:22 -0600

    Creacion de Forms para actualizacion de usuarios

---
+++ ProyectoMEIA/ProyectoMEIA/ActualizacionContraseña.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ProyectoMEIA
+{
+    partial class ActualizacionContraseña
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            btnCambiarContraseña = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(46, 77);
+            label1.Name = "label1";
+            label1.Size = new Size(158, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Ingrese su contraseña: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(46, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(201, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Ingrese su nueva contraseña: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(46, 200);
+            label3.Name = "label3";
+            label3.Size = new Size(214, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Confirme su nueva contraseña: ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(382, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(382, 130);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(382, 193);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // btnCambiarContraseña
+            // 
+            btnCambiarContraseña.Location = new Point(194, 263);
+            btnCambiarContraseña.Name = "btnCambiarContraseña";
+            btnCambiarContraseña.Size = new Size(175, 55);
+            btnCambiarContraseña.TabIndex = 6;
+            btnCambiarContraseña.Text = "Cambiar Contraseña";
+            btnCambiarContraseña.UseVisualStyleBackColor = true;
+            // 
+            // ActualizacionContraseña
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(578, 450);
+            Controls.Add(btnCambiarContraseña);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "ActualizacionContraseña";
+            Text = "ActualizacionContraseña";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button btnCambiarContraseña;
+    }
+}
